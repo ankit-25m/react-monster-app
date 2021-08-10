93d5785b1c165bdfbd279796d33fa1256a33977a
ankit-25m <mangalankit843@gmail.com>
2021-08-11 00:20:05 +0530

    my first commit reactapp

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }))
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     const { monsters, searchField } = this.state
     const filteredMonsters = monsters.filter((monster) =>
@@ -30,7 +34,7 @@ class App extends Component {
         <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder='search monster'
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
